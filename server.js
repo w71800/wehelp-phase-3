26e8799687bb1df54f7978921ec91d39afce6ef5
IKaRos <w71800@gmail.com>
2025-03-18 15:20:58 +0800

    chore: 調整了 server 的 cors 設定

--- server.js
+++ server.js
@@ -10,9 +10,17 @@ const listsApi = require('./api/lists.js')
 const messagesApi = require('./api/messages.js')
 const graphDataApi = require('./api/graphData.js')
 
+app.enable('trust proxy');
 
+const corsOptions = {
+  origin: '*',  // 或指定您的前端網域
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 app.use(express.static('dist'))
-app.use(cors())
+
 app.use("/api/list", listApi)
 app.use("/api/auth", authApi)
 app.use("/api/history", historyApi)
